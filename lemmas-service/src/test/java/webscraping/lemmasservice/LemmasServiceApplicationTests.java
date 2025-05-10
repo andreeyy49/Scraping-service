3d2295772cc5c91718a271cd4972b29a5580a944
andrei_kiashev <andreeyy49@gmail.com>
2025-05-11 01:22:35 +0300

    init lemmas service

---
+++ lemmas-service/src/test/java/webscraping/lemmasservice/LemmasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webscraping.lemmasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemmasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
